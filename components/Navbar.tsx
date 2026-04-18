13ed94eaf3167afad2a1f22c323153460e536a3a
Md Shoaib <email4966@gmail.com>
2026-04-18 06:50:45 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container px-12 z-30 py-2 shadow-xl bg-white rounded-full ring-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28">
-        <span>TRAVEL</span>
+        <span>TRAVELR</span>
       </Link>
       {/* DESKTOP */}
       <ul className="hidden lg:flex h-full">

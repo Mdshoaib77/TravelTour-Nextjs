1030323a6c9ffd5a17a3f27ef16cc540c746a6b0
Md Shoaib <email4966@gmail.com>
2026-04-22 10:59:04 +0600

    Update Testimonial.tsx

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -32,7 +32,7 @@ const Testimonial = () => {
       <h3 className="bold-32 text-center">Testimonial</h3>
       <p className="text-center max-w-lg m-auto text-gray-30 py-6">
        bk Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur iure
-        nulla repellat cupiditate incidunt eaque dolorem similiqueyoOh
+        nulla repellat cupiditate incidunt eaque dolorem similiqueyoOhj
       </p>
       <Slider {...settings}>
         {TESTIMONIAL.map((testimonial) => (

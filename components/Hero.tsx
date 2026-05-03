8b720029484614673d0da5ea21a2a7fbf149288c
Md Shoaib <email4966@gmail.com>
2026-05-03 07:17:40 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
 <p className="regular-16 mt-6 text-white lg:w-1/2">
   Explore the world bsest like never before witgh personaflized travel plans and curated experiences. sg
   Visdit ifconic landmgdarks, hiddedn treasures, and scenic escapes that inspire and dexcite. 
-  Enjoy smooth bookings fand reliable support.jo gfdsg bdd
+  Enjoy smooth bookings fand reliable support.jo gfdsg bddd
 </p>
       <div className="mt-8">
         <Button

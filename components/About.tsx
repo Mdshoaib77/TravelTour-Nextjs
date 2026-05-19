10de842578e34d3a0c32c27ebdd024e8d88a39bd
Md Shoaib <email4966@gmail.com>
2026-05-19 21:08:07 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
           <p className="text-gray-50">
            NO hgfdLorem ipsum nogfds tedyreolor sit amet consectetsusr adipisicing kihuielit.
             Perfernbcxvxehgdfndihggfsdss, sfdigdsllddum tempofrigdsbugdsfgds aliquid distinctio elriggendi nostrumf
-            porro molestiae ea, netrewcessyrteritatibuvffghdff
+            porro molestiae ea, netrewctrwessyrteritatibuvffghdff
           </p>
           <br />
           <p className="text-gray-50">

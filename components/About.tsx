0b42c35d734bc2774ee839a6e861e597e90e9ea6
Md Shoaib <email4966@gmail.com>
2026-05-21 19:40:18 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="flex flex-1 flex-col items-start justify-center">
           <h1 className="bold-52 capitalize pb-4">fk
            ks Join Us Inhdfyyreurt ytnbsExplhtewjgdfgoriyrtnsdag The Esndtirej hgdfWojrldjz gSmosskothly kzkexpgdfsefjddsjss
-          </h1>hdfghgdfsfnhgytreyhtwh g dfyrefgahyetyergfhgsdsytrereytrndd yer yer we gsd ew
+          </h1>hdfg df hgdfsfnhgytreyhtwh g dfyrefgahyetyergfhgsdsytrereytrndd yer yer we gsd ew
           <p className="text-gray-50">
            NO hgfdLorem ipsum nogfds tedyreolor sittw amet couern nsectetsusr adipisicing kihuielit.
             Perfernbcxvxehgdfndihhhtsgsggfsdss, sfdigds hdf llddum tempofrigd yer sbugdsfgds aliquid distinctio elriggendi nostrumf

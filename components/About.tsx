4ade7bc7214d904df05afa7792840a6c1cf88e94
Md Shoaib <email4966@gmail.com>
2026-05-26 03:29:18 +0600

    Fix text content in About.tsx
    Corrected text content in the About component.

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="flex flex-1 flex-col items-start justify-center">
           <h1 className="bold-52 capitalize pb-4">fk
            ks Join Us Inhdfyyreurt ytnbsExplhtewjgdfgoriyrtnsdag The Esndtirej hgdfWojrldjz gSmossk hfd vothly kzkexpgdfsefjddsjss
-          </h1>hdjgfg df hgfhgdfsjfgfnhg gfd ytreyhtwhyhdfter g dfyrefghdfahyetyergfhgsdsytrereytrndd yer yer we gsd ew
+          </h1>hdjgfg df hgfhgdfsjfgfnhg gfd ytreyhtwhyhdfter g ksisdfyrefghdfahyetyergfhgsdsytrereytrndd yer yer we gsd ew
           <p className="text-gray-50">
            NO hgfdLorem ipsum nogfds tedyreolor sittw amet couern nsectetsusr adipisicing kihuielit.
             Perfernbcxvxehgdfndihhhtsgsggfsdss, sfdigds hdf llddumisjsj tempohgfdfrigd yer sbugdsfgds aliquid distinctio elriggendi nostrumf

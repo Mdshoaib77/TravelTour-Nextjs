52609e51e35fa6135ae73cbaf8c060513aeec18a
Md Shoaib <email4966@gmail.com>
2026-05-27 19:57:31 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className="max-container padding-container relative top-28 sm:top-1/3 z-10">
       <h1 className="bold-44 sm:bold-64 text-white capitalize max-w-[36rem]">Explore The World With Us</h1>
 <p className="regular-16 mt-6 text-white lg:w-1/2">
-  Explore the world bsest like never before witgh personaflized tradvel plans and curated experiencehds. sg
+  Explore the world bsest like never before witgh personaflized ktradvel plans and curated experiencehds. sg
   Visdit ifconic landmgdarks, hiddedn treasures,d adnd jsnssdcednickks escadpgsdes that indspire and dexcite. 
   Enjoy smooth bookings fand reliable supportd.jo gfdsg bdddddddd g hg g
 </p>

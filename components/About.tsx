e4713eb653b0c83504465811151f7c418141a7f5
Md Shoaib <email4966@gmail.com>
2026-05-30 23:47:17 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
           </p>j 
           <br />
           <p className="text-gray-50">
-            noj Lorem ipsum dolhfdor sigdsyrtet amet co hdf njgfsectetur adipjisicking elit. Consecteturjsjj
+            noj Lorem ipsum dolhfdor sigdsyrtet amet co hdf njgfsecteturns adipjisicking elit. Consecteturjsjj
             error cohfdrrupthdf heytrex aphfderiam gdsdhfdolorjibusj erjhfgror corrupti jmex apueriamisis hrte
             dolorjibushfdkb
           </p>

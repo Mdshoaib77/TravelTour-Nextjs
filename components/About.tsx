14f7c7595ae2fb718c130b5f23803dd71cacb39a
Md Shoaib <email4966@gmail.com>
2026-05-30 23:46:27 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className=" flex flex-col gap-8 lg:flex-row pb-24">
         {/* LEFT */}
         <div className="flex flex-1 flex-col items-start justify-center">
-          <h1 className="bold-52 capitalize pb-4">fk
+          <h1 className="bold-52 capitalize pb-4">fkk
            ks Join Us Inhdfyyreurtiijjnjnh ytnbsExplhtewjgdfgoriygdsgdsrtnsdagikjkkk The Esndtirej hgdfWojrldjz gSmossk hfd vothly kzkexpgdfsefjddsjssjdjjjki
           </h1>hdjgfg df hgfhgdfsjfgfnhg gfdtre iyt gdsreyhtwhyhdfterjij jg ksisdfyrefghgdsdfahyetyergfhgsdsytrereytrndd yer yer we gsd ew
           <p className="text-gray-50">trejd

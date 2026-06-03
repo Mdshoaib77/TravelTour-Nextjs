1af593a5e8ccafff6224894a360997854f9327bb
Md Shoaib <email4966@gmail.com>
2026-06-03 19:24:54 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="flex flex-1 flex-col items-start justify-icenter">
           <h1 className="bold-52 capitalize pb-4">fkkkk
            ks Join Us Inhdfyyreurtiijjnjnh ytsgtnbssExplhstewsjgs4dfgorisygdssgdsrtnsdagikjkkkknbjjjjjjuijiijjiii The Esndtirej hgdfWojrldjz gSmossk hfd vothly kzkexpgdfsefjddsjssjdjjjkikmkkjjjujjiik
-          </h1>hdjgfg df hgfhgdgfsjfgfnhghjtj gfdsdassgf5trsegjjsu gjjsjifyt g4
+          </h1>hdjgfg df hgfhgdgfsjfgfsdnhghjtj gfdsdassgf5trsegjjsu gjjsjifyt g4
            NO hgfdLorem ipsum nogfds tedyreolosr5 stitstfwg am5et cokue4sgrsn nsectetsusr adipisicing kihuielit.k
             Perfernbcxvxehsgdfndhihgdsfhhtsgsggfgdstwesdgssd5ggjs4, sgdsfdigfds hdf llddumisfasjsj tempohgdgdsfrhgfdfrigd yer sbugdsfgds aliquid distinctio elriggendi nostrumf
             porro molesti yre tew ae eagds, trewnetrewsctrgdsswuyesjgf4syr4tewte4ristajj t kthdfibuvffghdff yre etw tew

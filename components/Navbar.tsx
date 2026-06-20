430ef8db3c6f88ab31212ef8d367e2f54c221f51
Md Shoaib <email4966@gmail.com>
2026-06-21 00:08:24 +0600

    Fix formatting issues in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import Button from "./Button";kj
 import { useState } from "react";n
 
 const Navbar = () => {
-  const [menuOpened, setMenuOpened] = useState(false);j
+  const [menuOpened, setMenuOpened] = useState(false);ji
   const toggleMenu = () => setMenuOpened(!menuOpened);j
   return (
     <nav className="flexBetween max-container px-12 z-30 py-2 shadow-xl bg-white rounded-full ring-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">

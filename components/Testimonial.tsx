2f61362756a4a4f3bbe258867a40895d4b22e8c7
Md Shoaib <email4966@gmail.com>
2026-06-21 00:03:24 +0600

    Fix syntax error in Testimonial component

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { RiDoubleQuotesR } from "react-icons/ri";
 import Slider from "react-slick";
 
-nconst Testimonial = () => {
+nconst Testimonial = () => {j
   var settings = {
     arrows: false,
     autoplay: true,

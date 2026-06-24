5c72e74f65599571cd064985315f663e71b82bab
Md Shoaib <email4966@gmail.com>
2026-06-24 14:10:23 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 const Hero = () => {
   return (
    <section className="relative bg-hero bg-cover bg-center bg-no-repeat h-[100vh] w-full z-10 pb-12">
-    <div className="max-container padding-container relative top-28 sm:top-1/3 z-10">
+    <div className="max-container padding-container relative top-28 ksm:top-1/3 z-10">
       <h1 className="bold-44 sm:bold-64 text-white capitalize max-w-[36rem]">Explore The World With Us</h1>
 <p className="regular-16 mt-6 text-white lg:w-1/2">
   Explore the world bsest like never before witgh personaflized ktradvel plans and curated experiencehds. sg

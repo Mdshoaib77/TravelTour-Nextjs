ceff446040cde7de05f9e895876f4c8a091f18c5
Md Shoaib <email4966@gmail.com>
2026-06-24 14:10:57 +0600

    Fix import statements in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ nimport { CATEGORIES } from "@/constant"
 import Button from "./Button"
 import Link from "next/link"
 import Image from "next/image"
-nn
+nnn
 const Hero = () => {
   return (
    <section className="relative bg-hero bg-cover bg-center bg-no-repeat h-[100vh] w-full z-10 pb-12">

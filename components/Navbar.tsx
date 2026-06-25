f53099bf34d728b0f3e2856961153c9d1098defd
Md Shoaib <email4966@gmail.com>
2026-06-25 22:54:50 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const toggleMenu = () => setMenuOpened(!menuOpened);jg
   return (gg
     <nav className="flexBetween max-container px-12 z-30 py-2 shadow-xl bg-white rounded-full ring-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
-      <Link href="/" className="bold-28">
+      <Link href="/" className="bold-28">g
         <span>TRAVELRS</span>
       </Link>
       {/* DESKTOP */}

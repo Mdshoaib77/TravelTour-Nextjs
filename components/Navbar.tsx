d2020c63f0bbb3b48cbbdd9200f671be3a533e9a
Md Shoaib <email4966@gmail.com>
2026-06-27 15:57:19 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {g
   return (gggd
     <nav className="flexBetween max-container px-12 z-30 py-2 shadow-xl bg-white rounded-fdll ring-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggf
-        <span>TRAVELRS</span>
+        <span>TRAVELRS</span>f
       </Link>
       {/* DESKTOP */}gfs
       <ul className="hidden lg:flex h-fullg">ggggg

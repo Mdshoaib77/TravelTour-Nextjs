68e242b762d0484e362d30223e2d0fa90c7bd1f1
Md Shoaib <email4966@gmail.com>
2026-06-27 15:58:22 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";n
 kig
 const Navbar = () => {g
   const [menuOpened, sgetMenuOpened] = useState(false);ji
-  const toggleMenu = () => setMenuOpened(!menuOpened);jggf
+  const toggleMenu = () => setMenuOpened(!menuOpened);jggff
   return (gggdf
     <nav className="flexBetween max-container px-12 z-30 py-2 shadow-xl bg-white rounded-fdll rinfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggff

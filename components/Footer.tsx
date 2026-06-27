a727ace94c8eb151728c5a715f1727f24c172242
Md Shoaib <email4966@gmail.com>
2026-06-27 16:01:22 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="flexCenter mb-24 pt-20">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href="/" className="mb-10 bold-20">
+          <Link href="/" className="mb-10 bold-20">d
             Yoghdss TdRAVdELSjdfdddsssshgdurttesdgsstjdeffgjsfsgasakd
           </Link>
           <div className="flex flex-wrap gap-8 sm:justify-between md:flex-1">

a5157cc97dc9d9af88655d4acde69227c60b2235
Md Shoaib <email4966@gmail.com>
2026-06-27 15:59:04 +0600

    Update Video.tsx

--- components/Video.tsx
+++ components/Video.tsx
@@ -5,7 +5,7 @@ const Video = () => {
   return (
     <section className='max-container padding-container py-16'>
       <div className='bg-video bg-cover bg-center bg-no-repeat h-[260px] md:h-[400px] xl:h-[500px] grid place-items-center rounded-2xl relative'>
-        <div className='h-16 w-16 bg-green-50 text-white bold-32 flexCenter rounded-full cursor-pointer z-10'><RiPlayFill/></div>
+        <div className='h-16 w-16 bg-green-50 text-white bold-32 flexCenter rounded-full cursor-pointer z-10d><RiPlayFill/></div>
         <div className='absolute h-12 w-12 bg-white rounded-full animate-ping'></div>
       </div>
     </section>

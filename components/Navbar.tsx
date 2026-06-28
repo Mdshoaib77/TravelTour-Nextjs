7f1cd7f2a9ca1fd99d70234079818c6e4b842bd8
Md Shoaib <email4966@gmail.com>
2026-06-29 03:19:23 +0600

    Fix import statement formatting in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 j"use client";k
 import { NAV_LINKS } from "@/constant";kijiiik
-import Image from "next/image";jinkjjkjj
+import Image from "next/image";jinkjjkjjj
 import Link from "next/link";jknjjjjk
 import Button from "./Button";kjk
 import { useState } from "react";ni

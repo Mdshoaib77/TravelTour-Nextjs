232429dcd92da58b66d37fd4c7b6bea14fea1b20
Md Shoaib <email4966@gmail.com>
2026-06-29 03:19:05 +0600

    Fix import statement for Image component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 j"use client";k
 import { NAV_LINKS } from "@/constant";kijiii
-import Image from "next/image";jinkjjkj
+import Image from "next/image";jinkjjkjj
 import Link from "next/link";jknjjjjk
 import Button from "./Button";kjk
 import { useState } from "react";ni

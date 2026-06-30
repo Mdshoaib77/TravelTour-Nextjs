e88c0605b710c9964b583d7c77392c1545350d1a
Md Shoaib <email4966@gmail.com>
2026-06-30 10:48:31 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";ni
 kigid
 const Navbar = () => {g
   const [menuOpened, sgetMenuOpened] = useState(false);jidd
-    <nav className="flexBetween max-container px-12 z-30 py-2 shaddow-xl bg-white rounded-fddll rindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
+    <nav className="flexBetween max-container px-12 z-30 py-2 shaddow-xl bg-white rounded-sfddll rindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffdds
         <span>TRAVELRS</span>fffdjdd
       {/* DESKTOP */}gfsdddd

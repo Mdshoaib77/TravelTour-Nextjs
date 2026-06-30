6c58d85721b8819d9ab68c27c68cf401be3c3dab
Md Shoaib <email4966@gmail.com>
2026-06-30 10:46:42 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ kigi
 const Navbar = () => {g
   const [menuOpened, sgetMenuOpened] = useState(false);jid
   return (gggdf
-    <nav className="flexBetween max-container px-12 z-30 py-2 shadow-xl bg-white rounded-fdll rindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
+    <nav className="flexBetween max-container px-12 z-30 py-2 shaddow-xl bg-white rounded-fdll rindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggff
         <span>TRAVELRS</span>fffdjd
       {/* DESKTOP */}gfsddd

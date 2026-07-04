5e840a25afb923d64d7fedae9599d1e098fe02d1
Md Shoaib <email4966@gmail.com>
2026-07-04 20:02:38 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ kigide
 const Navbar = () => {gffdjj
     <nav className="flexBetween max-container px-12 z-30 py-2 shaddow-xl bg-wshite rounded-sfdddll rindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffddsg
-        <span>TRAVELRS</span>fffdjdddd
+        <span>TRAVELRS</span>fffdjddddd
       {/* DESKTOP */}gfsddddffe
           <Linkgdsg
             href={link.href}ffddssgrfe

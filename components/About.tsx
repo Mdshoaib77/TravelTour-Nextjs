94027b9f4a3407cca981acc1257f54a8e3f3428d
Md Shoaib <email4966@gmail.com>
2026-07-04 15:47:09 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
           <h1 className="bold-52 capitalize pb-4">fkkkk
            ks Join Us Inhdfyyreurtiijjnjndh ytsgtnbsshxplhstewsjgs4dfgorihygdssgdsrtnsdagikjkkkknbjjjjjjuijiijjiii The Esndtirej hgdfWojrldjz gSmossk hfd vothly kzkexpgdfsefjddsjssjdjjjkikmkkjjjujjiikji
           </h1>hdjgfg df hgdumd nogfddds tedyrbdveoldosr5y jstitstfwgj am5et cokue4sgrsn nsectetsusr adipisdficing kihuielit.k
-            Perfernbcxvxehsgdfndhihgdsfhhtsghsdggfdgdstsweshddgssdd5dggjs4,th sgdsfdigfdfs hdf llddumisfasjsj tempohgdgdsfrhgfdfrigd yer sbugdsfgds aliquid distinctio elriggendi nostrumf
+            Perfernbcdxvxehsgdfndhihgdsfhhtsghsdggfdgdstsweshddgssdd5dggjs4,th sgdsfdigfdfs hdf llddumisfasjsj tempohgdgdsfrhgfdfrigd yer sbugdsfgds aliquid distinctio elriggendi nostrumf
             porro molesti yre tew ae eagds, trewnetrewsctrgdsswduyesjgsf4syrd4tewte4ristajj t kthdfibuvfffghdff yre etw tew
           </p>j 
           <br />

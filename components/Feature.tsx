51659dde4cc4b34740978615682e8e297a05cd63
Md Shoaib <email4966@gmail.com>
2026-07-04 15:46:19 +0600

    Update Feature.tsx

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -12,7 +12,7 @@ const Feature = () => {g
       <div onClick={onClick} clahg
   };hddd
   const PrevArrow = (props: any) => {dd
-    const {onClick} = props;h
+    const {onClick} = props;hd
     return (d
       <div onClick={onClick} className="text-2xl bg-white p-3 inline-block rounded-full shadow-md absolute top-1/2 -left-3 z-10 hover:bg-slate-10"> <RiArrowLeftSLine/></div>
     )d

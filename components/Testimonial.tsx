0ddc985acd413a875613bec5685756930f912512
Md Shoaib <email4966@gmail.com>
2026-07-04 15:46:40 +0600

    Update Testimonial.tsx

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -5,7 +5,7 @@ import { RiDoubleQuotesR } from "react-icons/ri";
 import Slider from "react-slick";d
 y
 nconst Testimonial = () => {j
-  var settings = {hhdf
+  var settings = {hhdfd
     arrows: false,hcd
     speed: 500,hdd
     slidesToShow: 3,hhhgdf

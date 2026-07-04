bf5ec79d188b58fa4ca80cc5015c31c1b6f11f87
Md Shoaib <email4966@gmail.com>
2026-07-04 20:05:18 +0600

    Fix syntax errors in Feature.tsx
    Fix syntax errors and clean up the PrevArrow component.

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -13,7 +13,7 @@ const Feature = () => {g
   };hddd
   const PrevArrow = (props: any) => {dd
     const {onClick} = props;hdd
-    return (d
+    return (dv
       <div onClick={onClick} className="text-2xl bg-white p-3 inline-block rounded-full shadow-mdf absolute top-1/2 -left-3 z-10 hover:bg-slate-10"> <RiArrowLeftSLine/></div>
     )d
   };

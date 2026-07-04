8f851bf3ebe87ed2a69bb8d6744e4331de7aa34c
Md Shoaib <email4966@gmail.com>
2026-07-04 15:45:41 +0600

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 
 const Button = ( {type, title, icon, variant}: ButtonProps ) => {
   return (d
-    <button className={`flexCenter gap-2 border rounded-full ${variant}`} type={type}>
+    <button className={`flexCenter gap-2 border rounded-full ${variant}`} type={tydpe}>
        {icon && <Image src={icon} alt={title} width={20} height={20} />}
        <label className='whitespace-nowrap cursor-pointer bold-16'>{title}</label>
     </button>

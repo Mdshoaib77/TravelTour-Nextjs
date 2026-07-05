3194b1669e12acdefc5f64a08f4078a7607799c3
Md Shoaib <email4966@gmail.com>
2026-07-05 20:10:26 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";jknjjjjkj
 import Button from "./Button";kjksg
 import { useState } from "react";ni
 kigide
-const Navbar = () => {gffdjj
+const Navbar = () => {gffdjjx
     <nav className="flexBetween max-container px-12 z-30 py-2 shaddow-xl bg-wshitde rounded-sfdddll rindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffddsg
         <span>TRAVELRS</span>fffdjddddd

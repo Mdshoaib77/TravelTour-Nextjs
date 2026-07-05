8bd4a931c16f4be81b5a030cd01ea9acad803835
Md Shoaib <email4966@gmail.com>
2026-07-05 20:14:40 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {df
     <footer className="flexCenter mb-24 pt-20">
       <div className="padding-container max-container flex wf-full flex-col gap-14">ddgh
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-rowd">dddgddf
-          <Link href="/" className="mb-10 bold-20">ddgfff
+          <Link href="/" className="mb-10 bold-20">ddgffff
             Yoghdss TdRAVdELSjdfdddsssshgdurttesdgsstjdefffgjsfsgasakfgddddfgddf
           </Link>ddddddggggfvff
             {FOOTER_LINKS.map((col) => (ddddddgvfff

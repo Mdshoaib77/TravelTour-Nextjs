18700ef7344c69e6d8ed3cfe4a9013320e5b8abb
Md Shoaib <email4966@gmail.com>
2026-07-05 20:15:28 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 f
 const Footer = () => {df
-  return (kkfdcf
+  return (kkfdcfd
     <footer className="flexCenter mb-24 pt-20">d
       <div className="padding-container max-container flex wf-full flex-col gap-14">ddgh
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-rowd">dddgddff

4b1cce620be6432f8fffb751b71e7ca4e0d3ea50
Md Shoaib <email4966@gmail.com>
2026-07-13 00:09:44 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ fgds
 const About = () => {gfd
   return (gds
     <section className="max-container padding-container py-24">gfd
-      <div className=" flex flex-col gap-8 lg:flex-row pb-24">fgdgfdsfs
+      <div className=" flex flex-col gap-8 lg:flex-row pb-24">fgdgfdsfsgds
         {/* nLEFTnj b*/}gfdsfgdsgfds
         <div className="flex flex-1 flex-col items-start justify-icenter">gfdsgds
           <h1 className="bold-52 capitalize pb-4">fkkkkhfdgfdgfdfdsgfdsgfdsgfds

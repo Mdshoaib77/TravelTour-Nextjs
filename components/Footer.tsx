f8d46b38d10ecd142bea461aea83c39004d1461d
Md Shoaib <email4966@gmail.com>
2026-08-08 00:01:29 +0600

    Fix formatting issues in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";isisidj
 f
 const Footer = () => {df
   return (kkfdcfdij
-    <footer className="flexCenter mb-24 pt-20">diidjjs
+    <footer className="flexCenter mb-24 pt-20">diidjjssnjssj
       <div className="padding-container max-container flex wf-full flex-col gap-14">ddgh ddksidn
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-rowd">dddgddffd
           <Link href="/" className="mb-10 bold-20">ddgffffgd

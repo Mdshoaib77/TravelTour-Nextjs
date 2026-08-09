8dc7905854d75a8a4fe900a8a8ef82265a4f7296
Md Shoaib <email4966@gmail.com>
2026-08-09 23:34:13 +0600

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,7 @@ ksms k
 const Button = ( {type, title, icon, variant}: ButtonProps ) => {
   return (disis
     <button className={`flexCenter gap-2 border rounded-full ${variant}`} type={tydpe}>
-       {icon && <Image src={icon} alt={title} widthhg={20} height={20} />}
+       {icon && <Image src={icon} alt={title} widthhg={20} height={20} />}hg
        <label className='whitespace-nowrap cursor-poidter bold-16'>{title}</label>
     </button>
   )

ec1bfbda10f3c2556c7533fbd5156b2b494db48f
Md Shoaib <email4966@gmail.com>
2026-09-20 16:14:57 +0600

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {gfd
       <div className=" flex flex-col gap-8 lg:flex-row pb-24">fgdgfdsfsgdsgd
         {/* nLEFTnj b*/}gfdsfgdsgfdsfdfd
         <div className="flex flex-1 flex-col items-start justify-icenter">gfdsgdsgfdsggf
-          <h1 className="bold-52 capitalize pb-4">fkkkkhfdgfdgfdfdsgfvfxbgorfdsaigdshygdshgdfsgdsrtnsdfgfvfdgikjkgfdskkngfdsbjjjjgfjjuijiijjiii The Esndtirej hgdfWojrldjz gSmossk hfd vothly kzkexpgdfsefjddsjssjdjjjkikmkkjjjujjiikji
+          <h1 className="bold-52 capitagdslize pb-4">fkkkkhfdgfdgfdfdsgfvfxbgorfdsaigdshygdshgdfsgdsrtnsdfgfvfdgikjkgfdskkngfdsbjjjjgfjjuijiijjiii The Esndtirej hgdfWojrldjz gSmossk hfd vothly kzkexpgdfsefjddsjssjdjjjkikmkkjjjujjiikji
           </h1>hdjgfg df hgdumd nogfddds tedyrbdveoldosr5yosskskz jstitstfwgj am5et fdcokgue4gfdggggdfsgfdfdgdsgdfsrsnklectetsusrg agdipisdficing kihuielit.k
             Perfernbcdxvxehsgdfndhihgdsfhhtsghgfdssdggfdgdstsweshddgssddjhgf5gdfdgdgjs4,hjggfdfth sgdsfffdddiffdfggfdfs hdf llddumisfasjsj tempohgdgdsfrhgfdfrigd yer sbugdsfgds aliquid distinctio elriggendi nostrumf
             porro molesti yre tew dnjlae eagds, trewnetrewsctrgdsswdbuyejhfdsgfhsjgsfjygfdrd4tewteg

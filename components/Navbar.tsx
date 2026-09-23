2596e608e3957c5244d139b52d218f977a0973e9
Md Shoaib <email4966@gmail.com>
2026-09-23 15:55:30 +0600

    Fix import statement for useState in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NAV_LINKS } from "@/constant";kijiiikjhhh
 import Image from "next/image";jinkjjkjjjjjkkkuuuuisjs
 import Link from "next/link";jknjjjjkjkzkz
 import Button from "./Button";kjksgfjssjjixkkx
-import { useState } from "react";nikeki ghds
+import { useState } from "react";nikeki ghdsgfsd
 kigidefgfdgfsd
 const Navbar = () => {gffdjjx
     <nav className="flexBetween max-container px-12 z-30 py-2 fdhaddow-xlfd gdsbg-wsfdhitdgfde rounded-sfdddll frindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">

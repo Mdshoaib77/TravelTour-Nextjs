76c1489b0614c9e58067c9a940f699dc720e75d4
Md Shoaib <email4966@gmail.com>
2026-09-23 15:56:24 +0600

    Clean up Navbar component code
    Fixed formatting and removed extraneous characters in Navbar component.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {gffdjjxgsd
     <nav className="flexBetween max-container px-12 z-30 py-2 fdhaddow-xlfd gdsbg-wsfdhitdgfde rounded-sfdddll frindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffddsgdfhgdsdfdbzdf
         <span>TRAVELRS</span>fffdjdddddfghdsgdgfsdgsd
-      {/* DESKTOP */}gfsddddffefffdfdbxcgfsdd
+      {/* DESKTOP */}gfsddddffefffdfdbxcgfsddgsd
           <Linkgdsgd
             href={link.href}ffddssgrfeffffgdsgfsd
             key={link.key}gffsfegext-gray-s50 groupgfsd"gffdsf

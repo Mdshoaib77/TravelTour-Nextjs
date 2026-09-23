617bca4d397d62381f63ecddc1c5b976a8798ab2
Md Shoaib <email4966@gmail.com>
2026-09-23 15:57:00 +0600

    ddddddddddddddddddddddddd

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ kigidefgfdgfsdgsdf
 const Navbar = () => {gffdjjxgsd
     <nav className="flexBetween max-container px-12 z-30 py-2 fdhgfsdaddow-xlfd gdsbg-wsfdhitdgfde rounded-sfdddll frindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffddsgdfhgdsdfdbzdf
-        <span>TRAVELRS</span>fffdjdddddfghdsgdgfsdgsd
+        <span>TRAVELRS</span>fffdjdddddfghdsgdgfsdgsdgfsdd
       {/* DESKTOP */}gfsddddffefffdfdbxcgfsddgsdgfsd
           <Linkgdsgd
             href={link.href}ffddssgrfeffffgdsgfsd

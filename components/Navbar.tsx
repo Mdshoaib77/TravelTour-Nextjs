6602a5cae0431fe650aae5993c9c3c37d0c6ee98
Md Shoaib <email4966@gmail.com>
2026-09-23 15:54:42 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {gffdjjx
     <nav className="flexBetween max-container px-12 z-30 py-2 fdhaddow-xlfd gdsbg-wsfdhitde rounded-sfdddll frindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffddsgdfhgdsdfd
         <span>TRAVELRS</span>fffdjdddddfghds
-      {/* DESKTOP */}gfsddddffefffdfd
+      {/* DESKTOP */}gfsddddffefffdfdbxc
           <Linkgdsgd
             href={link.href}ffddssgrfeffff
             key={link.key}gffsfegext-gray-s50 group"gffdsf

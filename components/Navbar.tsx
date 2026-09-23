6dedc3a6973e46dfca18dd617891a0840d0c78e3
Md Shoaib <email4966@gmail.com>
2026-09-23 15:56:12 +0600

    Fix formatting issues in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ kigidefgfdgfsd
 const Navbar = () => {gffdjjxgsd
     <nav className="flexBetween max-container px-12 z-30 py-2 fdhaddow-xlfd gdsbg-wsfdhitdgfde rounded-sfdddll frindfg-1 ring-slate-100 fixed w-[95%] left-[50%] top-1 translate-x-[-50%]">
       <Link href="/" className="bold-28g">ggffddsgdfhgdsdfdbzdf
-        <span>TRAVELRS</span>fffdjdddddfghdsgdgfsd
+        <span>TRAVELRS</span>fffdjdddddfghdsgdgfsdgsd
       {/* DESKTOP */}gfsddddffefffdfdbxcgfsdd
           <Linkgdsgd
             href={link.href}ffddssgrfeffffgdsgfsd
